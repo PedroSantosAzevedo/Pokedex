bba3dc4f41dc5de4f68bf136666a9a1da1737465
Pedro Azevedo <pedroazevedo@MacBook-Pro-de-Pedro.local>
2024-05-07 07:31:50 -0300

    initial project setup

---
+++ PokedexMvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokedexMvvm
+//
+//  Created by Pedro Azevedo on 07/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
