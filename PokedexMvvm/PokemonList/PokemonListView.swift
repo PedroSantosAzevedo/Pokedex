dcc7f37b4ee5dac1b09138e41d8e610f65f2b360
Pedro Azevedo <pedroazevedo@MacBook-Pro-de-Pedro.local>
2024-05-10 12:09:43 -0300

    added title

--- PokedexMvvm/PokemonList/PokemonListView.swift
+++ PokedexMvvm/PokemonList/PokemonListView.swift
@@ -10,6 +10,27 @@ import UIKit
 
 final class PokemonListView: UIView {
     
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = Theme.Colors.background.color
+        view.backgroundColor = .clear
+        view.alpha = 0.5
+        return view
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.alpha = 1
+        label.text = "Pokedex"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.textColor = Theme.Colors.onBackGround.color
+        label.font = Theme.Fonts.pokemonSolid.getFont(size: 50)
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
     lazy var tableView:UITableView = {
         let tableView = UITableView()
         addSubview(tableView)
@@ -25,6 +46,7 @@ final class PokemonListView: UIView {
     override init(frame: CGRect) {
         super.init(frame:frame)
         self.backgroundColor = Theme.Colors.background.color
+        setupView()
         
     }
     
@@ -32,20 +54,28 @@ final class PokemonListView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func layoutSubviews() {
-        setupView()
-    }
-    
 }
 
 extension PokemonListView: ViewCodeProtocol {
     
     func setupHierarchy() {
+        addSubview(containerView)
+        addSubview(titleLabel)
         addSubview(tableView)
     }
     
     func setupConstraints() {
-        tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        
+        containerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant:  -8).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        
+        tableView.topAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
         tableView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
